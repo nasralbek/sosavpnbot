6a91af590d899b5db54f89f465e6bec97eb53af9
WlcdWlcd <katsaq228@gmail.com>
2025-05-18 15:13:05 +0300

    constanats

--- modules/utils/constants.py
+++ modules/utils/constants.py
@@ -1,5 +1,15 @@
+from enum import Enum
 
 
+
+TELEGRAM_WEBHOOK = "/webhook"
 
 LOG_ZIP_ARCHIVE_FORMAT = "zip"
 LOG_GZ_ARCHIVE_FORMAT = "gz"
+
+
+class TransactionStatus(Enum):
+    PENDING = "pending"
+    COMPLETED = "completed"
+    CANCELED = "canceled"
+    REFUNDED = "refunded"
