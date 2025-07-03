42d3e53a3fc42890c6f75ef4ba51300f60bf7774
wlcd <artyomborisov11@gmail.com>
2025-07-03 19:07:50 +0300

    add vpn service

---
+++ modules/bot/services/vpn.py
@@ -0,0 +1,124 @@
+
+from datetime import datetime, timedelta
+import logging
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from config import Config
+from modules.database.models.user import User
+
+from remnawave_api import RemnawaveSDK
+from remnawave_api.models import CreateUserRequestDto, TelegramUserResponseDto, UpdateUserRequestDto, UsersResponseDto, UserResponseDto
+
+logger = logging.getLogger(__name__)
+
+class VPNService:
+    def __init__(
+        self,
+        config: Config,
+        session: async_sessionmaker,
+        #server_pool_service: ServerPoolService, TODO: MAKE
+    ) -> None:
+        self.config = config
+        self.session = session
+        self.r_sdk = RemnawaveSDK(base_url  = config.remnawave.PANEL_URL,
+                                  token     = config.remnawave.TOKEN)
+        #self.server_pool_service = server_pool_service
+        logger.info("VPN Service initialized.")
+
+    async def is_client_exists(self,user: User) -> bool:
+        client = await self.r_sdk.users.get_user_by_uuid(str(user.tg_id))
+        if not isinstance(client,UserResponseDto):
+            logger.info(f"Client {user.tg_id} not found")
+            return False
+        logger.info(f"Client {user.tg_id} found")
+        return True
+
+    async def get_client(self,user: User) -> UserResponseDto | None:
+        client = await self.r_sdk.users.get_user_by_uuid(str(user.tg_id))
+        if not isinstance(client,UserResponseDto):
+            logger.info(f"Client {user.tg_id} not found")
+            return None
+        logger.info(f"Client {user.tg_id} found")
+        return client
+
+    async def register_user(self,user: User) -> UserResponseDto | None:
+        
+        client = await self.r_sdk.users.create_user(
+           CreateUserRequestDto(username=user.username,
+                                expire_at=datetime.now(),
+                                telegram_id=user.tg_id,
+                                activate_all_inbounds=True,
+            ) 
+        )
+        if not isinstance(client,UserResponseDto):
+            logger.error(f"failed to register user : {user.tg_id}")
+            return None
+        return client
+
+    async def add_days(self,user: User, delta: timedelta) -> UsersResponseDto | None:
+        client = await self.get_client(user)
+        now = datetime.now()
+        if not isinstance(client,UserResponseDto):
+            logger.error(f"failed to get user {user.tg_id} while updating expiry time")
+            return
+        current_expiry = client.expire_at
+
+        if not isinstance(current_expiry,datetime):
+            current_expiry = now
+        if current_expiry<now:
+            current_expiry=now
+
+        new_expire_at = current_expiry + delta 
+
+        response = await self.r_sdk.users.update_user(
+            UpdateUserRequestDto(
+                uuid = client.uuid,
+                expire_at=new_expire_at
+            )
+        )
+        if not isinstance(response,UsersResponseDto):
+            logger.error("failed to send request, response not UsersResponseDto")
+            return None
+        logger.info(f"added delta to user: {user.tg_id}: {delta}")
+        return response
+    
+    async def get_key(self,user: User) -> str | None:
+        client = await self.get_client(user)
+        if not isinstance(client, UserResponseDto):
+            logger.error(f"failed to get user : {user.tg_id} while fetching key")
+            return
+        return str(client.subscription_uuid)
+
+    async def get_remaining_time(self,user: User) -> timedelta | None:
+        now = datetime.now()
+        expiry = self.get_expire_at(user)
+        if not isinstance(expiry,datetime):
+            return
+        remaining_time = expiry - now
+        return remaining_time
+
+    async def get_expire_at(self,user: User)-> datetime | None:
+        client = await self.get_client(user)
+ 
+        if not isinstance(client,UserResponseDto):
+            logger.error(f"failed to get user {user.tg_id}")
+            return None
+        if not isinstance(client.expire_at, datetime):
+            logger.error(f"failed to get user {user.tg_id}")
+            return None
+        
+        return client.expire_at
+               
+    async def set_expiry(self,user: User,new_expiry : datetime)-> UserResponseDto | None:
+        client = await self.get_client(user)
+        
+        response = await self.r_sdk.users.update_user(
+            UpdateUserRequestDto(
+                uuid = client.uuid,
+                expire_at=new_expiry
+            )
+        )
+        if not isinstance(response,UserResponseDto):
+            return None
+        return response
+
+
